4ae2335cdd411e9773f70817c1a8a15f7f3c86f0
Olivia Maltz <oliviamaltz@ist2-03112.apn.wlan.private.upenn.edu>
2024-11-14 14:49:49 -0500

    Containers for Loading and apply and quiz work in progress

--- pawsandfound/src/Quiz.js
+++ pawsandfound/src/Quiz.js
@@ -1,69 +1,60 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Quiz2 from './Quiz2';
-import Location from './Location'
+import { Link } from 'react-router-dom';
 import './Quiz.css';
 
 function Quiz() {
   return (
-    <div className="profile-container">
-      <h3>Would you like to foster a cat or a dog?</h3>
+    <div className="quiz-container">
+      {/* Question 1 */}
       <div className="quiz-item">
-      <div id="flex" class="d-flex justify-content-left">
-      <div class="form-check me-3">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckCat"/>
-        <label class="form-check-label" for="flexCheckCat">Cat</label>
-      </div>
-      <div class="form-check ms-3">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDog"/>
-        <label class="form-check-label" for="flexCheckDog">Dog</label>
-      </div>
-      </div>
-      </div>
-      
-      <div className="quiz-item">
-      <h3>Are you interested in fostering to adopt or temporary placement?</h3>
-      <div id="flex" class="d-flex justify-content-left">
-      <div class="form-check me-3">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckAdopt"/>
-        <label class="form-check-label" for="flexCheckAdopt">Foster to Adopt</label>
-      </div>
-      <div class="form-check ms-3">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckTemp"/>
-        <label class="form-check-label" for="flexCheckTemp">Temporary Foster</label>
-      </div>
-      </div>
-      </div>
-      
-      <div className="quiz-item">
-      <h3>Do you prefer a certain age range of animal?</h3>
-      <div class="container">
-      <div class="row justify-content-left">
-        <div class="col-md-6 text-left">
-        <div class="d-flex align-items-left">
-        <span class="me-2">0</span>
-        <input type="range" class="form-range w-100" id="rangeInput"></input>
-        <span class="ms-2">20</span>
-        </div>
+        <h3>Would you like to foster a cat or a dog?</h3>
+        <div id="flex" className="d-flex justify-content-left">
+          <div className="form-check me-3">
+            <input className="form-check-input" type="checkbox" id="flexCheckCat" />
+            <label className="form-check-label" htmlFor="flexCheckCat">Cat</label>
+          </div>
+          <div className="form-check ms-3">
+            <input className="form-check-input" type="checkbox" id="flexCheckDog" />
+            <label className="form-check-label" htmlFor="flexCheckDog">Dog</label>
+          </div>
         </div>
       </div>
+
+      {/* Question 2 */}
+      <div className="quiz-item">
+        <h3>Are you interested in fostering to adopt or temporary placement?</h3>
+        <div id="flex" className="d-flex justify-content-left">
+          <div className="form-check me-3">
+            <input className="form-check-input" type="checkbox" id="flexCheckAdopt" />
+            <label className="form-check-label" htmlFor="flexCheckAdopt">Foster to Adopt</label>
+          </div>
+          <div className="form-check ms-3">
+            <input className="form-check-input" type="checkbox" id="flexCheckTemp" />
+            <label className="form-check-label" htmlFor="flexCheckTemp">Temporary Foster</label>
+          </div>
+        </div>
       </div>
+
+      {/* Question 3 */}
+      <div className="quiz-item">
+        <h3>Do you prefer a certain age range of animal?</h3>
+        <div>
+          <span className="me-2">0</span>
+          <input type="range" className="form-range w-100" id="rangeInput" />
+          <span className="ms-2">20</span>
+        </div>
       </div>
 
-      <Link to="/Location">
-        <button className="footer-button">Back</button>
-      </Link>
-
-      <Link to="/Quiz2">
-          <button className="footer-button">Next</button>
-      </Link>
-
-      <Routes>
-        <Route path="/Location/*" element={<Location />} />
-        <Route path="/Quiz2/*" element={<Quiz2 />} />
-      </Routes>
+      {/* Back and Next Buttons */}
+      <div className="quiz-button-container">
+        <Link to="/Location">
+          <button className="quiz-footer-button">Back</button>
+        </Link>
+        <Link to="/Quiz2">
+          <button className="quiz-footer-button">Next</button>
+        </Link>
+      </div>
     </div>
-
   );
 }
 
